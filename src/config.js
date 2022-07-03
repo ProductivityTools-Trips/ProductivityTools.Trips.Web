f66cbe2a8366efa6b16172a09142d114946eed6d
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-07-03 13:22:18 +0200

    config

--- src/config.js
+++ src/config.js
@@ -2,4 +2,8 @@ const dev = {
     PATH_BASE: 'https://localhost:7042/api'
 }
 
-export const config = dev;
+const prd = {
+    PATH_BASE: 'https://apitrips.productivitytools.tech:8070/api'
+}
+
+export const config = process.env.NODE_ENV === 'development' ? dev : prd;
