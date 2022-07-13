f73a08730aaaaf91540b06f1c2e144ff7618626e
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-07-13 07:47:06 +0200

    trip list

--- src/Components/TripList/index.js
+++ src/Components/TripList/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import service from '../../services/apiService'
 import {Link} from 'react-router-dom'
+import moment from 'moment';
 
 function TripList() {
 
@@ -18,14 +19,30 @@ function TripList() {
     return (
         <div>
         
-            <p>Trips:</p>
+            <p>Trip1s:</p>
             <table>
                 <tbody>
+                    <tr>
+                        <th>TripId</th>
+                        <th>Name</th>
+                        <th>Days</th>
+                        <th>Nights</th>
+                        <th>Start</th>
+                        <th>End</th>
+                        <th>Cost</th>
+                        <th>Expensed</th>
+                    </tr>
                     {trips && trips.map(x => {
                         return (
                             <tr key={x.tripId}>
                                 <td><Link to={"tripdetail/"+x.tripId}>{x.tripId}</Link></td>
                                 <td>{x.name}</td>
+                                <td>{x.days}</td>
+                                <td>{x.nights}</td>
+                                <td>{moment(x.start).format('YYYY.MM.DD')}</td>
+                                <td>{moment(x.end).format('YYYY.MM.DD')}</td>
+                                <td>{x.cost}</td>
+                                <td>{x.expensed}</td>
                             </tr>)
                     })}
                 </tbody>
