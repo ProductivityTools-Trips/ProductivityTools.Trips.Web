91bc0d19523ac414c1f10f2bb17095422d3aa18b
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-07-15 09:31:46 +0200

    from to

--- src/Components/TripAdd/index.js
+++ src/Components/TripAdd/index.js
@@ -1,11 +1,21 @@
 import { useState } from "react";
 import service from "../../services/apiService";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
-function TripAdd(){
+import moment from 'moment';
 
-    const [trip, setTrip]=useState(null);
+
+import TextField from '@mui/material/TextField';
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+
+function TripAdd() {
+
+    const [trip, setTrip] = useState({ start: moment().format('yyyy-MM-DD'), end: moment().format('yyyy-MM-DD') });
     const navigate = useNavigate();
+
 
     const handleChange = (e) => {
         console.log(e);
@@ -17,17 +27,41 @@ function TripAdd(){
         }))
     }
 
-    
+
     const addTrip = async () => {
         await service.addTrip(trip);
         navigate('/', { replace: true })
     }
 
 
-    return(
+    return (
         <div>
-             <p><span>Trip Name: </span><input type='edit' name='name' value={trip && trip.name || ""} onChange={handleChange}></input></p>
-             <input type="button" value="Add" onClick={addTrip}></input>
+            <p><span>Trip Name: </span><input type='edit' name='name' value={trip && trip.name || ""} onChange={handleChange}></input></p>
+            <LocalizationProvider dateAdapter={AdapterMoment}>
+                <DatePicker
+                    label="From"
+                    value={trip.startDate}
+                    onChange={(newValue) => {
+                        console.log(newValue.format());
+                        setTrip(prevState => ({
+                            ...prevState, start: newValue.format('yyyy-MM-DD')
+                        }));
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+                <DatePicker
+                    label="To"
+                    value={trip.endDate}
+                    onChange={(newValue) => {
+                        console.log(newValue.format());
+                        setTrip(prevState => ({
+                            ...prevState, end: newValue.format('yyyy-MM-DD')
+                        }));
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+            </LocalizationProvider>
+            <input type="button" value="Add" onClick={addTrip}></input>
         </div>
     )
 }
