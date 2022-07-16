46c08b32d4afd99019fd12a92d5de89d2a22f7bb
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-07-16 14:11:41 +0200

    adding trip

--- src/Components/TripAdd/index.js
+++ src/Components/TripAdd/index.js
@@ -10,7 +10,7 @@ import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
-moment.locale('pl', { week: { dow: 1 } })
+moment.locale('en', { week: { dow: 1 } })
 
 function TripAdd() {
 
@@ -26,6 +26,8 @@ function TripAdd() {
         setTrip(prevState => ({
             ...prevState, [name]: value
         }))
+
+        console.log(trip);
     }
 
 
@@ -34,50 +36,65 @@ function TripAdd() {
         navigate('/', { replace: true })
     }
 
-    const closed=async()=>{
-        // let s=moment(trip.start)  
-        // console.log(s);
-        // let e=moment(trip.end)
-        // let d=e.diff(s,'days');
-        // setTrip(prevState => ({
-        //     ...prevState, 'days': d
-        // }))
-        // console.log(trip);
+    const calculateDays = async (s1, e1) => {
+
+        // let s = moment(trip.start)
+        // console.log('start', s);
+        // console.log(s1);
+        // let e = moment(trip.end)
+        // console.log('end', e);
+        // console.log(e1);
+        let n = e1.diff(s1, 'days');
+        let d = n + 1;
+
+        console.log('dff', d);
+        setTrip(prevState => ({
+            ...prevState, 'days': d
+        }))
+        setTrip(prevState => ({
+            ...prevState, 'nights': n
+        }))
+        console.log(trip);
     }
 
 
     return (
         <div>
-            <p><TextField label="Trip Name" name="name" value={trip && trip.name || ""} onChange={handleChange}></TextField></p>
+            <p><TextField label="Trip Name" name="name" value={trip && trip.name || ''} onChange={handleChange}></TextField></p>
             <LocalizationProvider dateAdapter={AdapterMoment}>
                 <p><DatePicker
                     label="From"
-                    value={trip.startDate}
+                    value={trip.start}
+                    inputFormat='yyyy.MM.DD'
                     onChange={(newValue) => {
                         console.log(newValue.format());
                         setTrip(prevState => ({
                             ...prevState, start: newValue.format('yyyy-MM-DD')
                         }));
+
+                        calculateDays(newValue, trip.end);
+
                     }}
-                    onClose={closed}
                     renderInput={(params) => <TextField {...params} />}
                 /></p>
                 <p><DatePicker
                     label="To"
-                    value={trip.endDate}
+                    value={trip.end}
+                    inputFormat='yyyy.MM.DD'
                     onChange={(newValue) => {
                         console.log(newValue.format());
                         setTrip(prevState => ({
                             ...prevState, end: newValue.format('yyyy-MM-DD')
                         }));
+                        console.log(trip);
+                        calculateDays(trip.start, newValue);
                     }}
-                    onClose={closed}
                     renderInput={(params) => <TextField {...params} />}
                 />
                 </p>
             </LocalizationProvider>
-            <p><TextField label="Days" type="number" name="days" onChange={handleChange} value={trip.days||''}></TextField></p>
-            <p><TextField label="Nights" type="number" name="nights" onChange={handleChange} value={trip.nights||''}></TextField></p>
+            <p><TextField label="Days" type="number" name="days" onChange={handleChange} value={trip.days || 0}></TextField></p>
+            <p><TextField label="Nights" type="number" name="nights" onChange={handleChange} value={trip.nights || 0}></TextField></p>
             <input type="button" value="Add" onClick={addTrip}></input>
         </div>
     )
