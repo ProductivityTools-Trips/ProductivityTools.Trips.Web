1981726d0246dcc9c7bdf45e11536c03ba99afc3
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-08-04 07:07:47 +0200

    sorting

--- src/Components/TripList/index.js
+++ src/Components/TripList/index.js
@@ -8,16 +8,37 @@ import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 function TripList() {
 
 
-    const [trips, setTrips] = useState(null)
+    const [trips, setTrips] = useState(null);
+    const [order, setOrder] = useState('asc');
 
     useEffect(() => {
         const fetchData = async () => {
             const r = await service.getTripsFullView();
-            const sorted=r.sort((a,b)=>(a.start > b.start ? -1 : 1))
+            const sorted = r.sort((a, b) => (a.start > b.start ? -1 : 1))
             setTrips(sorted);
         }
         fetchData();
     }, [])
+
+    const setSortedField = (field) => {
+
+        if (order == 'asc') { setOrder('desc') } else { setOrder('asc') }
+
+        const t1 = [...trips]
+        debugger;
+        const sorted = t1.sort((a, b) => {
+            if (a[field] > b[field]) {
+                return (order == 'asc' ? 1 : -1)
+
+            }
+            else {
+                return (order == 'desc' ? 1 : -1)
+            }
+        })
+        setTrips(sorted);
+        console.log(trips);
+    }
+
 
 
     return (
@@ -28,29 +49,29 @@ function TripList() {
             <table className='green'>
                 <tbody>
                     <tr>
-                        <th>Name</th>
-                        <th>Days</th>
-                        <th>Nights</th>
-                        <th>Start</th>
-                        <th>End</th>
-                        <th>Cost</th>
-                        <th>Expensed</th>
+                        <th><button onClick={() => setSortedField('name')}>Name</button></th>
+                        <th><button onClick={() => setSortedField('start')}>Start</button></th>
+                        <th><button onClick={() => setSortedField('end')}>End</button></th>
+                        <th><button onClick={() => setSortedField('days')}>Days</button></th>
+                        <th><button onClick={() => setSortedField('nights')}>Nights</button></th>
+                        <th><button onClick={() => setSortedField('cost')}>Cost</button></th>
+                        <th><button onClick={() => setSortedField('expensed')}>Expensed</button></th>
                     </tr>
                     {trips && trips.map(x => {
                         return (
                             <tr key={x.tripId}>
                                 <td><Link to={"tripdetail/" + x.tripId}>{x.name}</Link></td>
-                                <td>{x.days}</td>
-                                <td>{x.nights}</td>
                                 <td>{moment(x.start).format('YYYY.MM.DD')}</td>
                                 <td>{moment(x.end).format('YYYY.MM.DD')}</td>
+                                <td>{x.days}</td>
+                                <td>{x.nights}</td>
                                 <td>{x.cost && (x.cost).toLocaleString(undefined, { maximumFractionDigits: 0 })}</td>
                                 <td>{x.expensed && (x.expensed).toLocaleString(undefined, { maximumFractionDigits: 0 })}</td>
                             </tr>)
                     })}
                 </tbody>
             </table>
-    
+
         </container>
     )
 }
