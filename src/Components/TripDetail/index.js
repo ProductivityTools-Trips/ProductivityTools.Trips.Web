8cf3b133e2501fafd9e01c165f737aa335ffa6bd
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-09-05 22:57:57 +0200

    trip detail

--- src/Components/TripDetail/index.js
+++ src/Components/TripDetail/index.js
@@ -1,11 +1,22 @@
+import { useEffect, useState } from 'react'
 import { Link, useParams } from "react-router-dom";
 import ExpenseList from "../ExpenseList";
 import JournalList from "../JournalList";
+import service from '../../services/apiService'
+
 
 
 function TripDetail() {
 
     let params = useParams();
+    const [trip, setTrip] = useState(null);
+    useEffect(() => {
+        const fetchData = async () => {
+            const r = await service.getTrip(params.id);
+            setTrip(r);
+        }
+        fetchData();
+    }, [])
 
     return (
         <div>
@@ -14,6 +25,7 @@ function TripDetail() {
             <Link to={"/expenseadd/?tripId=" + params.id}>AddExpense</Link>
             <Link to={"/journaladd/?tripId=" + params.id}>Add Notes</Link>
             <p>TripDetail</p>
+            <p>{trip.description}</p>
             <ExpenseList></ExpenseList>
             <JournalList></JournalList>
         </div>)
